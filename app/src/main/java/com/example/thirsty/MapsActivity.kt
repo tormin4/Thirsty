2909b710d24c1bc455e161401c3997a5112b43cb
hehe <wisest2092@gmail.com>
2022-03-19 16:48:39 +0000

    Initial Setup

--- app/src/main/java/com/example/thirsty/MapsActivity.kt
+++ app/src/main/java/com/example/thirsty/MapsActivity.kt
@@ -41,8 +41,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val winnipeg = LatLng(-49.89, -97.14)
+        mMap.addMarker(MarkerOptions().position(winnipeg).title("Marker in Wpg"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(winnipeg))
     }
 }
