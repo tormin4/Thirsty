17ab6f1318ec16922fb400d18681de40eb075bb1
tormi <tormin4@gmail.com>
2022-03-20 12:48:34 -0500

    Added like some dumb hollow rectangle thing Added front end Added progressbar Finalized the search and marker stuff

--- app/src/main/java/com/example/thirsty/MapsActivity.kt
+++ app/src/main/java/com/example/thirsty/MapsActivity.kt
@@ -1,24 +1,37 @@
 package com.example.thirsty
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
-
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import androidx.appcompat.app.AppCompatActivity
 import com.example.thirsty.databinding.ActivityMapsBinding
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.PointOfInterest
+import com.google.android.gms.maps.model.*
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPoiClickListener {
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPoiClickListener  {
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var marker: Marker
+    private var TAG = "Info"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
+        super.onCreate(savedInstanceState)
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -26,6 +39,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPoiCli
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        autoCompleteSearch()
     }
 
     /**
@@ -43,22 +57,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPoiCli
         map.setOnPoiClickListener(this)
 
         val options = GoogleMapOptions()
-        options.mapType(GoogleMap.MAP_TYPE_SATELLITE)
+        options.mapType(GoogleMap.MAP_TYPE_NORMAL)
             .compassEnabled(false)
             .rotateGesturesEnabled(false)
             .tiltGesturesEnabled(false)
 
         // Add a marker in Sydney and move the camera
-        val winnipeg = LatLng(49.89, -97.14)
-        map.addMarker(MarkerOptions().position(winnipeg).title("Marker in Wpg"))
+        val winnipeg = LatLng(49.884444, -97.146389)
+        marker = map.addMarker(MarkerOptions().position(winnipeg).title("Destination"))!!
+        marker.setVisible(false);
 
         cameraConfig()
     }
 
     private fun cameraConfig(){
         map.setMinZoomPreference(8.0f)
-        map.setMaxZoomPreference(15.0f)
-
+        map.setMaxZoomPreference(20.0f)
 
         val winnipegBounds = LatLngBounds(
             LatLng((49.718), -97.409),  // SW bounds
@@ -76,4 +90,49 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnPoiCli
         ).show()
     }
 
+    private fun autoCompleteSearch(){
+        //Initialize the SDK and create a new Places Client instance
+        if(!Places.isInitialized()) {
+            Places.initialize(applicationContext, "AIzaSyBN2RH2u8Hs-t47sTYnFQSc7_rPZwsxQXo");
+        }
+
+        // Initialize the placesClient
+        val placesClient = Places.createClient(this)
+
+        // Initialize the AutocompleteSupportFragment.
+        val autocompleteFragment =
+            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                    as? AutocompleteSupportFragment
+
+        // Setting location bias to Winnipeg
+        autocompleteFragment?.setLocationBias(RectangularBounds.newInstance(
+            LatLng(49.884444, -97.146389),
+            LatLng(49.884444, -97.146389)
+        ))
+
+        //Selecting a country
+        autocompleteFragment?.setCountries("CA")
+
+        // Specify the types of place data to return.
+        autocompleteFragment?.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment?.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                val latiLongi = place.latLng
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(latiLongi!!, 13.0f))
+                marker.remove()
+                marker.setVisible(true)
+                marker = map.addMarker(MarkerOptions().position(latiLongi).title("Destination"))!!
+                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+            }
+            override fun onError(status: Status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: $status")
+            }
+        })
+    }
+
 }
+
